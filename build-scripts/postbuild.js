a33a014407af4019beebf885278518249163a06b
webtoast <webtoast@gmail.com>
2021-11-29 19:47:25 -0600

    chore: create production files

--- build-scripts/postbuild.js
+++ build-scripts/postbuild.js
@@ -1,11 +1,27 @@
 // Core modules
 const fs = require('fs');
+const path = require('path');
 
-console.log('Files to process for release');
-fs.readdir('.build-cache', (err, folders) => {
-    folders.forEach((folder) => {
-        fs.readdir('.build-cache/' + folder, (err, files) => {
-            console.log(files);
-        })
-    })
-});
+// Public modules
+const prettify = require('postcss-prettify');
+
+// Code
+const version = JSON.parse(fs.readFileSync('./package.json', 'utf-8')).version;
+const versionStr = '/*! v' + version + '*/';
+const buildCacheDir = '.build-cache';
+
+try {
+    const css = fs.readFileSync(`${buildCacheDir}/scss/style.css`, 'utf-8');
+    const js = fs.readFileSync(`${buildCacheDir}/js/global.js`, 'utf-8');
+    const cssDist = versionStr + css;
+    const jsDist = versionStr + js;
+
+    fs.mkdirSync(path.join(__dirname, '../dist'));
+    fs.writeFileSync('./dist/global.min.css', cssDist);
+    fs.writeFile('./dist/global.css', prettify.process(cssDist).css, (err) => {
+        return err;
+    });
+    fs.writeFileSync('./dist/global.js', jsDist);
+} catch(error) {
+    console.log('error', error);
+}
